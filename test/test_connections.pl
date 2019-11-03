8bb3cd0dc7f88d234839c129f6704be970dc4241
alrickemilien <aemilien@stduent.42.fr>
2019-11-03 19:25:27 +0100

    ++ run tests

--- test/test_connections.pl
+++ test/test_connections.pl
@@ -1,41 +1,38 @@
 #!/usr/bin/perl
 
-# use strict;
-# use warnings;
+use strict;
+use warnings;
 
 my $pid = fork();
-print "MYPMID IS $pid";
+
 if ($pid != 0)
 {
-    print 'pid', $pid;
-
     open(my $fd, '>', 'ircserver.pid') or die "Could not open pid file for irc server: $!";
-
     print $fd $pid;
-
     close($fd);
+
+    setpgrp(0, 0);
 
     # This is the child process.
     # exec() the external program.
-    exec("./build/server") or die "Could not run server: $!";
-}
-elsif($pid == 0)
-{
-    print "Waiting ...\n";
-    sleep(300);
-    print "Closing server\n";
+    exec("build/server") or die "Could not run server: $!";
 
-    open(my $pidfd, 'ircserver.pid') or die "Can't read server pid file: $!\n";  
-    $pid_server = <$pidfd>;
-    close (my $pidfd);  
-    print "Killing $pid_server";
-
-    # waitpid($pid_server, WNOHANG);    
+    exit
 }
-else
+elsif(!defined($pid))
 {
         die "could not fork";
 }
 
-# if ($pid != 0){
-# } 
+print "Waiting ...\n";
+sleep(5);
+
+open(my $pidfd, 'ircserver.pid') or die "Can't read server pid file: $!\n";  
+my $pidserver = <$pidfd>;
+close($pidfd);
+print "Killing $pidserver\n";
+kill 9, -$pidserver;
+
+# Killing self
+kill 9, -$pid;
+waitpid $pid, 0;
