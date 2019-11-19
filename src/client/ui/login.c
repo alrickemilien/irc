18011b7eec6d231aedbcb38fc8c4d5cbd17a3713
alrickemilien <aemilien@student.42.fr>
2019-11-19 16:59:28 +0100

    Little review login

--- src/client/ui/login.c
+++ src/client/ui/login.c
@@ -29,35 +29,18 @@ void login_window_init(t_env *e)
 
 void login_connect(t_env *e, const char *uname, const char *pword)
 {
-    char concat[512];
-
-    // if (options->ipv6 == 1)
-    //     client_ipv6(options, e);
-    // else
-    //     client_ipv4(options, e);
-
     if (e->sock == -1)
     {
         logerrno("main:");
         return;
     }
 
-    // loginfo("options.command: %s\n", options.command);
+    c2s_pass(e, e->sock, pword);
+    c2s_nick(e, e->sock, uname);
+    c2s_connect(e, e->sock, pword);
 
-    // memset(e.fds[e.sock].nickname, 0, NICKNAMESTRSIZE + 1);
-    // memset(e.fds[e.sock].username, 0, USERNAMESTRSIZE + 1);
-    // memset(e.fds[e.sock].channelname, 0, CHANNELSTRSIZE + 1);
-
-    // execute_precommands(&options, &e);
-
-    // do_select(&options, &e);
-
-    memset(concat, 0, sizeof(concat));
-
-    sprintf(concat, "PASS %s\x0D\x0ANICK %s\x0D\x0AUSER %s %s :%s\x0D\x0A",
-            pword, uname, uname, "lalal@lalal.com", "Diego delavega");
-
-    send(e->sock, concat, sizeof(concat), 0);
+    if (e->sock != -1)
+        XSAFE(-1, close(e->sock), "login_connect::close");
 
     /* received flag */
     // recv(socket_fd, &flag, 2, 0);
@@ -66,9 +49,10 @@ void login_connect(t_env *e, const char *uname, const char *pword)
     // if (0 == flag)
     // { /* 0 is login success, -1 is failed */
 
-    gtk_label_set_text(GTK_LABEL(label), "loading...");
+    // gtk_label_set_text(GTK_LABEL(label), "loading...");
 
     gtk_widget_hide(window);
+
     // mainWindow();
     // }
     // else
@@ -77,18 +61,12 @@ void login_connect(t_env *e, const char *uname, const char *pword)
     // 			"loading failed,username or password error!");
     //     gtk_entry_set_text(GTK_ENTRY(passwordEntry), "");
     // }
-
-    do_select(e);
-
-    if (e->sock != -1)
-        XSAFE(-1, close(e->sock), "login_connect::close");
 }
 
 /*
  * Name: get_login_info
  * Description: get username and password from ui from,
  * 		call login_connect() function.
- *
  */
 void get_login_info(GtkWidget *widget, gpointer data)
 {
