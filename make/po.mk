3006a65751b7e230176432844aa1f54196c69a28
alrickemilien <aemilien@student.42.fr>
2019-12-04 14:20:20 +0100

    Fix relink on po make

--- make/po.mk
+++ make/po.mk
@@ -3,9 +3,10 @@ PO_BUILD_PREFIX=build/locale/
 PO_SRC=ar.po \
 	fr.po \
 	he.po
-PO=$(patsubst %.po, %.mo, $(PO_SRC))
 
-%.mo: $(PO_SRC_PREFIX)%.po
-	@mkdir -p $(PO_BUILD_PREFIX)$(patsubst $(PO_SRC_PREFIX)%,%,$(basename  $<))/LC_MESSAGES
-	msgfmt $< -o $(PO_BUILD_PREFIX)$(patsubst $(PO_SRC_PREFIX)%,%,$(basename  $<))/LC_MESSAGES/irc.mo
+MO=$(patsubst %.po, $(PO_BUILD_PREFIX)/%/LC_MESSAGES/irc.mo, $(PO_SRC))
+
+$(PO_BUILD_PREFIX)%/LC_MESSAGES/irc.mo: $(PO_SRC_PREFIX)%.po
+	@mkdir -p $(basename  $@)
+	@msgfmt $< -o $@
 
