0c4ec8eed4ccce7e574229f01e03b2d24900f45a
Alrick EMILIEN <aemilien@e2r12p3.42.fr>
2019-12-14 19:43:32 +0100

    Some compilation fixes

--- make/assets.mk
+++ make/assets.mk
@@ -4,7 +4,7 @@ ASSETS_SRC=icons8-annuler-16.png \
 			icons8-ok-16.png \
 			icons8-mode-veille-16.png \
 			icons8-grand-hashtag-40.png \
-			icons8-haute-priorité-100.png \
+			icons8-haute-priority-100.png \
 			icons8-info-carré-16.png \
 			icons8-dormir-16.png \
 			icons8-importer-24.png
