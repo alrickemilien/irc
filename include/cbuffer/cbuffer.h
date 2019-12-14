adc1d7d74cc81c8d3e07eb40bbc0faf4b376ed6e
Alrick EMILIEN <aemilien@e3r10p4.42.fr>
2019-12-14 15:12:28 +0100

    End norme

--- include/cbuffer/cbuffer.h
+++ include/cbuffer/cbuffer.h
@@ -1,30 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cbuffer.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aemilien <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/14 15:11:20 by aemilien          #+#    #+#             */
+/*   Updated: 2019/12/14 15:11:22 by aemilien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CBUFFER_H
-#define CBUFFER_H
+# define CBUFFER_H
 
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <stdarg.h>
+# include <stdbool.h>
+# include <stddef.h>
+# include <stdint.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <stdarg.h>
 
-#define CBUFFSIZE 512
+# define CBUFFSIZE 512
 
-typedef struct  s_cbuffer {
-    uint8_t     buffer[CBUFFSIZE];
-    size_t      head;
-    size_t      tail;
-    bool        full;
-}               t_cbuffer;
+typedef struct	s_cbuffer {
+	uint8_t		buffer[CBUFFSIZE];
+	size_t		head;
+	size_t		tail;
+	bool		full;
+}				t_cbuffer;
 
 /*
 ** Reset the circular buffer to empty, head == tail. Data not cleared
 ** Requires: cbuf is valid and created by circular_buf_init
 */
 
-void cbuffer_reset(t_cbuffer* cbuf);
+void			cbuffer_reset(t_cbuffer *cbuf);
 
 /*
 ** Continues to add data if the buffer is full
@@ -32,9 +44,11 @@ void cbuffer_reset(t_cbuffer* cbuf);
 ** Requires: cbuf is valid and created by circular_buf_init
 */
 
-void cbuffer_put(t_cbuffer *cbuf, const uint8_t *data, size_t n);
+void			cbuffer_put(
+		t_cbuffer *cbuf, const uint8_t *data, size_t n);
 
-int cbuffer_putstr(t_cbuffer *cbuf, const char *str);
+int				cbuffer_putstr(
+		t_cbuffer *cbuf, const char *str);
 
 /*
 ** Rejects new data if the buffer is full
@@ -42,7 +56,8 @@ int cbuffer_putstr(t_cbuffer *cbuf, const char *str);
 ** Returns 0 on success, -1 if buffer is full
 */
 
-int circular_buf_put_safe(t_cbuffer* cbuf, const uint8_t data);
+int				circular_buf_put_safe(
+		t_cbuffer *cbuf, const uint8_t data);
 
 /*
 ** Checks if the buffer is empty
@@ -50,38 +65,40 @@ int circular_buf_put_safe(t_cbuffer* cbuf, const uint8_t data);
 ** Returns true if the buffer is empty
 */
 
-bool cbuffer_isempty(t_cbuffer* cbuf);
+bool			cbuffer_isempty(t_cbuffer *cbuf);
 
 /*
 ** Check the number of elements stored in the buffer
 ** Requires: cbuf is valid and created by circular_buf_init
 ** Returns the current n
 */
-size_t cbuffer_size(const t_cbuffer* cbuf);
+size_t			cbuffer_size(const t_cbuffer *cbuf);
 
 /*
 ** Set n bytes using send() command
 */
 
-int cbuffer_send(int cs, t_cbuffer *cbuf, size_t n, int flags);
-int cbuffer_send_until_str(int cs, t_cbuffer *cbuf, const char *str, int flags);
+int				cbuffer_send(
+		int cs, t_cbuffer *cbuf, size_t n, int flags);
+int				cbuffer_send_until_str(
+		int cs, t_cbuffer *cbuf, const char *str, int flags);
 
 /*
-** Fill buffer as much as possible using with recv 
+** Fill buffer as much as possible using with recv
 */
 
-int cbuffer_recv(t_cbuffer *cbuf, int cs);
-int cbuffer_read(t_cbuffer *cbuf, int cs);
+int				cbuffer_recv(t_cbuffer *cbuf, int cs);
+int				cbuffer_read(t_cbuffer *cbuf, int cs);
 
-size_t cbuffer_indexof(t_cbuffer *cbuf, const char *str);
+size_t			cbuffer_indexof(t_cbuffer *cbuf, const char *str);
 
-void cbuffer_dropn(t_cbuffer *cbuf, size_t n);
-void cbuffer_drop_until(t_cbuffer *cbuf, const char *str);
+void			cbuffer_dropn(t_cbuffer *cbuf, size_t n);
+void			cbuffer_drop_until(t_cbuffer *cbuf, const char *str);
 
-void cbuffer_debug(const t_cbuffer *cbuf);
+void			cbuffer_debug(const t_cbuffer *cbuf);
 
-int cbuffer_putcmd(t_cbuffer *cbuf, const char *fmt, ...);
+int				cbuffer_putcmd(t_cbuffer *cbuf, const char *fmt, ...);
 
-int cbuffer_putvcmd(t_cbuffer *cbuf, const char *fmt, va_list ap);
+int				cbuffer_putvcmd(t_cbuffer *cbuf, const char *fmt, va_list ap);
 
 #endif
--- include/cbuffer/cbuffer_ssl.h
+++ include/cbuffer/cbuffer_ssl.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cbuffer_ssl.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aemilien <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/14 15:11:52 by aemilien          #+#    #+#             */
+/*   Updated: 2019/12/14 15:11:53 by aemilien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CBUFFER_SSL_H
-#define CBUFFER_SSL_H
+# define CBUFFER_SSL_H
 
-#include <openssl/crypto.h>
-#include <openssl/err.h>
-#include <openssl/pem.h>
-#include <openssl/rsa.h>
-#include <openssl/ssl.h>
-#include <openssl/x509.h>
+# include <openssl/crypto.h>
+# include <openssl/err.h>
+# include <openssl/pem.h>
+# include <openssl/rsa.h>
+# include <openssl/ssl.h>
+# include <openssl/x509.h>
 
-#include <cbuffer/cbuffer.h>
+# include <cbuffer/cbuffer.h>
 
-int cbuffer_read_ssl(t_cbuffer *cbuf, SSL *ssl);
-int cbuffer_write_ssl(SSL *ssl, t_cbuffer *cbuf, size_t n);
+int	cbuffer_read_ssl(t_cbuffer *cbuf, SSL *ssl);
+int	cbuffer_write_ssl(SSL *ssl, t_cbuffer *cbuf, size_t n);
 
 #endif
